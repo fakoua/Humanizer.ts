155d631e7f072ed2f6cfc41e35ce4248e241468d
Sameh Fakoua <sameh.osd@novopath.com>
2020-01-02 23:09:50 +0200

    adding tests

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Humanizer.ts
+
 Humanizer meets all your TypeScript needs for manipulating and displaying strings, dates, times, timespans, numbers and quantities.
--- test.ts
+++ test.ts
@@ -1,31 +1,7 @@
-import './src/Humanizer/Extensions/ByteSize.ts'
-import './src/Humanizer/Extensions/ToQuantity.ts'
-import './src/Humanizer/Extensions/Vocabularies.ts'
-import './src/Humanizer/Extensions/Ordinalize.ts'
+import "./src/Humanizer/Bytes/ByteSize_test.ts"
+import "./src/Humanizer/Inflections/Ordinalize_test.ts"
+import "./src/Humanizer/Inflections/Vocabularies_test.ts"
 
-function testBytes() {
-    let fileSize = (10).kilobytes()
-    console.log(fileSize.bits)
-    console.log(fileSize.bytes)
-    console.log(fileSize.kilobytes)
-    console.log(fileSize.megabytes)
-    console.log(fileSize.gigabytes)
-    console.log(fileSize.terabytes)
-}
+import { runTests } from "./test_deps.ts"
 
-function testQuatity() {
-    console.log("case".toQuantity(0))
-    console.log("case".toQuantity(1))
-    console.log("case".toQuantity(2))
-}
-
-function testVocap() {
-    console.log("Man".pluralize())
-    console.log("women".singularize())
-}
-
-function testOrd() {
-    console.log("2".ordinalize())
-    console.log((5).ordinalize())
-}
-testOrd()
+runTests()
